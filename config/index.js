a36d13a73337ffe21b8a427e595079495a14bde9
Cvetelina Mihaylova <cvetelinapm@abv.bg>
2020-10-26 19:37:34 +0200

    port changed

--- config/index.js
+++ config/index.js
@@ -2,7 +2,7 @@ const env = process.env.NODE_ENV || 'development';
 
 const config = {
     development: {
-        port: process.env.PORT || 3000,
+        port: process.env.PORT || 9999,
         dbUrl: 'mongodb://127.0.0.1:27017/shoeShelter',
         cookie: 'x-auth-token',
         secret: 'superSecret',
