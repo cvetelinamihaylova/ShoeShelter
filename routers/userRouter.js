b19157ed20a6227f19d21a3c456b88302eb52043
Cvetelina Mihaylova <cvetelinapm@abv.bg>
2020-10-26 19:40:07 +0200

    added routs for post method

--- routers/userRouter.js
+++ routers/userRouter.js
@@ -3,5 +3,10 @@ const { userController } = require('../controllers');
 module.exports = (router) => {
     router.get('/login', userController.get.login);
     router.get('/register', userController.get.register);
+    router.get('/profile', userController.get.profile);
+
+    router.post('/login', userController.post.login);
+    router.post('/register', userController.post.register);
+
     return router;
 };
