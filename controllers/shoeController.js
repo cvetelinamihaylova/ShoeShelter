dcb8dd3153b8abf6c63a005a19b148cdc46934f5
Cvetelina Mihaylova <cvetelinapm@abv.bg>
2020-10-31 11:32:10 +0200

    implemented all requests

--- controllers/shoeController.js
+++ controllers/shoeController.js
@@ -1,16 +1,71 @@
+const { shoeModel } = require('../models');
+
 module.exports = {
-    get:{
-        all(req, res, next){
-            res.render('./shoes/shoes.hbs')
+    get: {
+        all(req, res, next) {
+            shoeModel.find({})
+                // .populate('accessories')
+                .lean()
+                .then((shoes) => {
+                    console.log(shoes)
+                    res.render('./shoes/shoes.hbs', { shoes })
+                })
+                .catch(next);
         },
-        create(req, res, next){
+        create(req, res, next) {
             res.render('./shoes/create.hbs')
         },
-        details(req, res, next){
-            res.render('./shoes/details.hbs')
+        details(req, res, next) {
+            const id = req.params.id;
+            shoeModel
+                .findOne({ _id: id })
+                .lean()
+                .then(shoe => {
+                    res.render('./shoes/details.hbs', shoe)
+                })
+                .catch(next);
         },
-        edit(req, res, next){
-            res.render('./shoes/edit.hbs')
+        edit(req, res, next) {
+            const id = req.params.id;
+            shoeModel
+                .findOne({ _id: id })
+                .lean()
+                .then(shoe => {
+                    console.log(shoe)
+                    console.log(shoe)
+                    res.render('./shoes/edit.hbs', shoe);
+                })
+                .catch(next);
         },
+        delete(req, res, next) {
+            const id = req.params.id;
+
+            shoeModel
+                .deleteOne({ _id: id })
+                .then(() => {
+                    res.redirect('/shoes/all');
+                })
+                .catch(next)
+        },
+    },
+    post: {
+        create(req, res, next) {
+            shoeModel
+                .create({ ...req.body, seller: req.user })
+                .then(() => {
+                    res.redirect('/shoes/all');
+                })
+                .catch(next);
+        },
+        edit(req, res, next) {
+            const id = req.params.id;
+
+            shoeModel
+                .updateOne({ _id: id }, { $set: { ...req.body } })
+                .then(() => {
+                    res.redirect(`/shoes/details/${id}`);
+                })
+                .catch(next);
+        }
     }
 };
