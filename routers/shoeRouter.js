bd6f65571e549de56ba5cfdeb4dc34f44d8f1277
Cvetelina Mihaylova <cvetelinapm@abv.bg>
2020-10-31 11:34:01 +0200

    implemented all routes

--- routers/shoeRouter.js
+++ routers/shoeRouter.js
@@ -3,8 +3,12 @@ const { shoeController } = require('../controllers');
 module.exports = (router) => {
     router.get('/all', shoeController.get.all);
     router.get('/create', shoeController.get.create);
-    router.get('/edit', shoeController.get.edit);
-    router.get('/details', shoeController.get.details);
+    router.get('/edit/:id', shoeController.get.edit);
+    router.get('/details/:id', shoeController.get.details);
+    router.get('/delete/:id', shoeController.get.delete);
+
+    router.post('/create', shoeController.post.create);
+    router.post('/edit/:id', shoeController.post.edit);
 
     return router;
 };
--- routers/userRouter.js
+++ routers/userRouter.js
@@ -4,6 +4,7 @@ module.exports = (router) => {
     router.get('/login', userController.get.login);
     router.get('/register', userController.get.register);
     router.get('/profile', userController.get.profile);
+    router.get('/logout', userController.get.logout);
 
     router.post('/login', userController.post.login);
     router.post('/register', userController.post.register);
