af53fc00ac49dc3d350bf0950ee749ad45b779fb
Cvetelina Mihaylova <cvetelinapm@abv.bg>
2020-10-31 11:31:12 +0200

    added route for authentication

--- config/express.js
+++ config/express.js
@@ -1,12 +1,14 @@
 const expressHandlebars = require('express-handlebars');
 const cookieParser = require('cookie-parser');
+const { auth } = require('../utils');
 
 module.exports = (express, app) => {
     app.use(express.static('public'));
     app.use(express.json());
-    app.use(express.urlencoded({extended: true}));
+    app.use(express.urlencoded({ extended: true }));
     app.use(cookieParser());
-
+    app.use(auth);
+    
     app.engine('hbs', expressHandlebars({
         layoutsDir: 'views',
         defaultLayout: 'base-layout.hbs',
