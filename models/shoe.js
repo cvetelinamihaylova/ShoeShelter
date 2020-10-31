c017867cb5caa10138f1a9395fe0525eeedb6506
Cvetelina Mihaylova <cvetelinapm@abv.bg>
2020-10-31 11:32:53 +0200

    init

--- models/shoe.js
+++ models/shoe.js
@@ -1,9 +1,38 @@
 module.exports = (mongoose) => {
     const { Schema } = mongoose;
-    const { String, ObjectId } = Schema.Types;
+    const { String, ObjectId, Number } = Schema.Types;
 
     const shoeSchema = new Schema({
-
+        name: {
+            type: String,
+            required: true
+        },
+        price: {
+            type: Number,
+            required: true
+        },
+        imageURL: {
+            type: String,
+            required: true
+        },
+        brand: {
+            type: String,
+            required: true
+        },
+        description: {
+            type: String,
+            required: true
+        },
+        seller: {
+            type: String,
+            required: true
+        },
+        buyers: [
+            {
+                type: ObjectId,
+                ref: 'User'
+            }
+        ],
     });
 
     return mongoose.model('Shoe', shoeSchema);
