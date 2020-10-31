f99e8a6625f2f356568d8690ca5c8b937bece293
Cvetelina Mihaylova <cvetelinapm@abv.bg>
2020-10-31 11:33:29 +0200

    added functionality for pass crypting

--- models/user.js
+++ models/user.js
@@ -1,3 +1,6 @@
+const bcrypt = require('bcrypt');
+const { saltRounds } = require('../config');
+
 module.exports = (mongoose) => {
     const { Schema } = mongoose;
     const { String, ObjectId } = Schema.Types;
@@ -23,6 +26,30 @@ module.exports = (mongoose) => {
             }
         ]
     });
+    userSchema.methods.comparePasswords = function (providedPassword) {
+        return new Promise((resolve, reject) => {
+            bcrypt.compare(providedPassword, this.password, function (err, result) {
+                if (err) { reject(err); return; }
+                resolve(result);
+            });
+        });
+    };
+    userSchema.pre('save', function (done) {
+        const user = this;
 
+        if (!user.isModified('password')) {
+            done();
+            return;
+        }
+
+        bcrypt.genSalt(saltRounds, (err, salt) => {
+            if (err) { done(err); return; }
+            bcrypt.hash(user.password, salt, (err, hash) => {
+                if (err) { done(err); return; }
+                user.password = hash;
+                done();
+            });
+        });
+    });
     return mongoose.model('User', userSchema);
 }
